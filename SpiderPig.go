c3c3e6b6300090fd5616d49583236e13cbf993b4
Theodore Rolle <theodoremrolle@students.abtech.edu>
2018-03-28 17:15:51 -0700

    Fixed url context bug

--- SpiderPig.go
+++ SpiderPig.go
@@ -16,57 +16,61 @@ func main(){
 	seedUrl := *s
 	var newUrls, oldUrls []url.URL
 	newUrls = append(newUrls, seedUrl)
-	oldUrls = append(oldUrls, seedUrl)
 
 	for len(newUrls) > 0 {
 		var currentUrl url.URL
 		currentUrl, newUrls = newUrls[0], newUrls[1:]	//Pop the first entry in the list of URL's to visit
-		//fmt.Println("Scraping page: ", currentUrl.String())
+		fmt.Println("Scraping page: ", currentUrl.String())
 		pageLinks := ScrapeLinks(currentUrl)	//Get all the URL's on target page
-		//fmt.Println("Found ",len(pageLinks), " links, processing...")
-		for _/*in*/,itm := range pageLinks {
-			//fmt.Println(itm.String())
+		fmt.Println("Found ",len(pageLinks), " links, processing...")
+		for in,itm := range pageLinks {
+			fmt.Println(itm.String())
 			if itm.Hostname() != currentUrl.Hostname(){	//Validate inside domain
-				//fmt.Println("Link ",in," is outside of the domain")
+				fmt.Println("Link ",itm," is outside of the domain")
 				continue
 			}
 			if findUrl(itm, oldUrls) {	//validate not processed
-				//fmt.Println("Link ",in," is already seen")
+				fmt.Println("Link ",in," is already seen")
 				continue
 			}
 			if findUrl(itm, newUrls) {	//validate not already queued
-				//fmt.Println("Link ",in," is already queued")
+				fmt.Println("Link ",in," is already queued")
 				continue
 			}
 			if !((itm.Scheme == "http") || (itm.Scheme == "https")) {	//validate not some javascript bullshit
-				//fmt.Println("Link ",in," is not an http link")
+				fmt.Println("Link ",itm," is not an http link")
 				continue
 			}
 			res, _ := http.Head(itm.String())	//probe for header
-			switch res.Header.Get("Content-Type") {
+			
+			ctype := res.Header.Get("Content-Type")
+			isHtml, _ := regexp.MatchString("html", ctype)
+			if isHtml {
+				ctype = "text/html"
+			}
+			
+			switch ctype {
+			case "":
+				fmt.Println("Link ",itm," does not have a content type tag. Adding to queue anyway.")
+				newUrls = append(newUrls,itm)
 			case "audio/x-wav":	
 				fmt.Println("Link ",itm," is an audio file, downloading...")
 				DownloadFile(itm.EscapedPath()[1:],itm)	//download if wav
 				fmt.Println("Download complete")
 			case "text/html":
-				//fmt.Println("Link ",in," is a new html file, adding ",itm.String()," to queue")
+				fmt.Println("Link ",in," is a new html file, adding ",itm.String()," to queue")
 				newUrls = append(newUrls, itm) //add to queue if valid html
-			case "application/xhtml+xml":
-				//fmt.Println("Link ",in," is a new html file, adding ",itm.String()," to queue")
-				newUrls = append(newUrls, itm)
-			case "text/html;charset=utf-8":
-				//fmt.Println("Link ",in," is a new html file, adding ",itm.String()," to queue")
-				newUrls = append(newUrls, itm)
 			default:
 				fmt.Println("Link ",itm," is a file of type ",res.Header.Get("Content-Type"),", ignoring")
 			}
 		}
-		//fmt.Println("done.\n")
+		fmt.Println("done.\n")
 		oldUrls = append(oldUrls, currentUrl)
-		//fmt.Println("URL's to explore: ")
-		//for in,itm := range newUrls {
-			//fmt.Println(in,": ",itm.String())
-		//}
+		/*fmt.Println("URL's to explore: ")
+		for in,itm := range newUrls {
+			fmt.Println(in,": ",itm.String())
+		}
+		*/
 
 	}
 
@@ -95,7 +99,7 @@ func ScrapeLinks(targetUrl url.URL) []url.URL {		//Gets all the links on a page,
 
 	switch {
 	case tokenType == html.ErrorToken:
-		//fmt.Println("End of file")
+		fmt.Println("End of file")
 		return output
 
 	case tokenType == html.StartTagToken:
@@ -104,9 +108,11 @@ func ScrapeLinks(targetUrl url.URL) []url.URL {		//Gets all the links on a page,
 			//fmt.Println("Link Found")
 				for _, attribute := range currentToken.Attr {
 					if attribute.Key == "href" {
-						//fmt.Println("URL=",attribute.Val)
+						fmt.Println("URL=",attribute.Val)
+						removeHashThingy := regexp.MustCompile(`#[^#]*$`)
+						urlWithoutHashThingy := removeHashThingy.ReplaceAllString(attribute.Val, "")
+						u, err := targetUrl.Parse(urlWithoutHashThingy)
 
-						u, err := url.Parse(attribute.Val)
 						if err != nil{
 							log.Fatal(err)
 						}
@@ -123,6 +129,7 @@ func ScrapeLinks(targetUrl url.URL) []url.URL {		//Gets all the links on a page,
 				}
 		}
 	}
+
 	return output
 
 }
@@ -130,17 +137,17 @@ func ScrapeLinks(targetUrl url.URL) []url.URL {		//Gets all the links on a page,
 func DownloadFile(filepath string, targetUrl url.URL) error {
 
     // Create the file
-	//fmt.Println("Download function called on filepath ",filepath," with url ",targetUrl)
+	fmt.Println("Download function called on filepath ",filepath," with url ",targetUrl)
    	rootDirRegexp := regexp.MustCompile(`/[^/]*$`)
 	rootDir := rootDirRegexp.ReplaceAllString(filepath, "")
-	//fmt.Println("making root dir ",rootDir)
+	fmt.Println("making root dir ",rootDir)
 	err := os.MkdirAll(rootDir, os.ModePerm)
 	if err != nil {
 		fmt.Println("error")
     	fmt.Println(err)
         return err
     }
-	//fmt.Println("making file ",filepath)
+	fmt.Println("making file ",filepath)
     out, err := os.Create(filepath)
     if err != nil {
     	fmt.Println("error")
@@ -150,7 +157,7 @@ func DownloadFile(filepath string, targetUrl url.URL) error {
     defer out.Close()
 
     // Get the data
-    //fmt.Println("Getting data from ",targetUrl.String)
+    fmt.Println("Getting data from ",targetUrl.String())
     resp, err := http.Get(targetUrl.String())
     if err != nil {
     	fmt.Println("error")
@@ -160,7 +167,7 @@ func DownloadFile(filepath string, targetUrl url.URL) error {
     defer resp.Body.Close()
 
     // Write the body to file
-    //fmt.Println("Writing data to file...")
+    fmt.Println("Writing data to file...")
     _, err = io.Copy(out, resp.Body)
     if err != nil {
     	fmt.Println("error")
